5cf35c0fcdeb36063668180cf6b261c2a2127036
Frolov Kirill <frolovkirill2116@mail.ru>
2026-06-11 23:30:19 +0500

    Добавил SqliteDiskRepository

--- src/sqlite_disk_repository.py
+++ src/sqlite_disk_repository.py
@@ -1,23 +1,55 @@
 from typing import List, Dict, Optional
+import sqlite3
 
 class SqliteDiskRepository:
     def __init__(self, db_path: str):
-        pass
+        self.conn = sqlite3.connect(db_path)
+        self.conn.row_factory = sqlite3.Row
+        self._init_tables()
     
     def _init_tables(self):
-        pass
+        cur = self.conn.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS disks (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    mount_point TEXT NOT NULL,
+                    total_space INTEGER NOT NULL,
+                    used_space INTEGER NOT NULL DEFAULT 0,
+                    reserved_space INTEGER NOT NULL DEFAULT 0,
+                    status TEXT NOT NULL DEFAULT 'available'
+                    )''') 
+        self.conn.commit()
 
     def add_disk(self, mount_point: str, total_space: int, status: str = "available") -> int:
-        pass
+        cur = self.conn.cursor()
+        cur.execute('''INSERT INTO disks (mount_point, total_space, used_space, reserved_space, status) VALUES (?, ?, ?, ?, ?)''', (mount_point, total_space, 0, 0, status))
+        self.conn.commit()
+        return cur.lastrowid
 
     def get_disk(self, disk_id: int) -> Optional[Dict]:
-        pass
+        cur = self.conn.cursor()
+        cur.execute('''SELECT * 
+                    FROM disks 
+                    WHERE id = ?''', [disk_id])
+        row = cur.fetchone()
+        return dict(row) if row else None
 
     def get_all_disks(self) -> List[Dict]:
-        pass
+        cur = self.conn.cursor()
+        cur.execute('''SELECT * 
+                    FROM disks''')
+        rows = cur.fetchall()
+        return [dict(row) for row in rows]
 
     def update_disk_space(self, disk_id: int, used_delta: int, reserved_delta: int) -> None: #used_delta - изменение used_space, reserved_delta - изменение reserved_space
-        pass
+        cur = self.conn.cursor()
+        cur.execute('''UPDATE disks 
+                    SET used_space = used_space + ?, reserved_space = reserved_space + ? 
+                    WHERE id = ?''', [used_delta, reserved_delta, disk_id])
+        self.conn.commit()
 
     def update_disk_status(self, disk_id: int, status: str) -> None:
-        pass
+        cur = self.conn.cursor()
+        cur.execute('''UPDATE disks 
+                    SET status = ?
+                    WHERE id = ?''', [status, disk_id])
+        self.conn.commit()
